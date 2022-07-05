fe01dac84d9bcc865f1808a4cd93ae9850dd6540
logm <luiz-augusto.gomes@gft.com>
2022-07-05 13:27:46 -0300

    Create Exceptions

---
+++ src/main/java/br/com/gft/exception/EntityNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.gft.exception;
+
+public class EntityNotFoundException extends LojaException {
+
+    private static final long serialVersionUID = 8766566498150867601L;
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
+
