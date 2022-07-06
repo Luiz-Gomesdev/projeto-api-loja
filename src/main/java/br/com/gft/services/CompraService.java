727d9d4dc05579e886276bc97a415d722b67d9be
logm <luiz-augusto.gomes@gft.com>
2022-07-06 15:15:20 -0300

    Updated services classes

--- src/main/java/br/com/gft/services/CompraService.java
+++ src/main/java/br/com/gft/services/CompraService.java
@@ -1,11 +1,11 @@
 package br.com.gft.services;
 
-import br.com.gft.entities.*;
+import br.com.gft.entities.Compra;
+import br.com.gft.entities.Usuario;
 import br.com.gft.repositories.CompraRepository;
-import br.com.gft.repositories.EstoqueRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class CompraService {
@@ -26,40 +26,11 @@ public class CompraService {
 
         compra.getItens()
                 .stream()
-                .forEach(i -> estoqueService.acrescentaQuantidadeEstoque(i.getProduto().getId(), compra.getFilial().getId(), i.getQuantidade()));
+                .forEach(i -> estoqueService.acrescentaQuantidadeEstoque(i.getProduto()
+                        .getId(), compra.getFilial().getId(), i.getQuantidade()));
 
 
         return compraRepository.save(compra);
     }
 
-    @Service
-    public static class EstoqueService {
-
-        private final EstoqueRepository estoqueRepository;
-
-        public EstoqueService(EstoqueRepository estoqueRepository) {
-            this.estoqueRepository = estoqueRepository;
-        }
-
-        public Integer acrescentaQuantidadeEstoque(Long produtoId, Long filialId, Integer quantidade) {
-
-            Optional<Estoque> op_estoque = estoqueRepository.findByProduto_IdAndFilial_Id(produtoId, filialId);
-
-            if(op_estoque.isPresent()) {
-
-                Estoque estoque = op_estoque.get();
-                estoque.setQuantidade(estoque.getQuantidade() + quantidade);
-
-                return estoqueRepository.save(estoque).getQuantidade();
-            }
-
-            Estoque estoque = new Estoque();
-            estoque.setProduto(new Produto(produtoId));
-            estoque.setFilial(new Filial(filialId));
-            estoque.setQuantidade(quantidade);
-
-            return estoqueRepository.save(estoque).getQuantidade();
-
-        }
-    }
 }
--- src/main/java/br/com/gft/services/UsuarioService.java
+++ src/main/java/br/com/gft/services/UsuarioService.java
@@ -2,6 +2,9 @@ package br.com.gft.services;
 
 import br.com.gft.entities.Usuario;
 import br.com.gft.repositories.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
